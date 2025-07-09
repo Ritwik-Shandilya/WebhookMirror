e9d33f453601b73cca8534dd341377461dbb751a
Ritwik Shandilya <111673038+Ritwik-Shandilya@users.noreply.github.com>
2025-07-09 22:44:36 +0530

    Serve React build from Rails

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
     get "endpoints/by_uuid/:uuid", to: "endpoints#show_by_uuid"
     resources :endpoints, only: [ :create, :index, :update, :destroy ] do
       resources :requests, only: [ :index, :create ] do
-        delete '/', to: 'requests#destroy_all', on: :collection
+        delete "/", to: "requests#destroy_all", on: :collection
       end
     end
     resources :requests, only: [ :show ]
   end
 
-  match "/:uuid", to: "capture#receive", via: :all
+  match "/:uuid", to: "capture#receive", via: :all, constraints: { uuid: /[0-9a-fA-F\-]{36}/ }
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # root "posts#index"
+  root "static#index"
+  get "*path", to: "static#index"
 end
